d97f66b1a6f76ea0149abf5aab13e47522e6c4e8
Костя Жихарев <kostyazhikharev@gmail.com>
2020-10-18 12:02:55 +0300

    Bind test with lib

--- src/RockScissorsPaperTests/WinerCheckerTest.cs
+++ src/RockScissorsPaperTests/WinerCheckerTest.cs
@@ -1,14 +1,15 @@
 using System;
 using Xunit;
+using RockScissorsPaperLib;
 
 namespace RockScissorsPaperTests
 {
-    public class UnitTest1
+    public class WinerCheckerTest
     {
         [Fact]
         public void Test1()
         {
-
+            WinerChecker winerChecker = new WinerChecker();
         }
     }
 }
